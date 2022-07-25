95db73b39aeb0f63682a1ac426fd698ed4973a0b
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-07-25 14:07:27 -0300

    Create sequencia.py

---
+++ sequencia.py
@@ -0,0 +1,10 @@
+# Faça um programa em Python que some uma sequência de números.
+print("Digite uma sequência de valores terminada por zero.")
+soma=0
+valor=1
+while valor!=0:
+    valor=int(input("Digite um valor a ser somado "))
+    soma=soma+valor
+print(soma)
+# O resultado dos testes com seu programa foi:
+# Parabéns! Todos os testes tiveram sucesso!
